de380921d4455eada0adfef349b378f504f57757
Kayleigh Nielsen <purplekitti909@gmail.com>
2025-02-06 10:58:28 +0100

    row counter functionality

---
+++ src/components/RowCounter.jsx
@@ -0,0 +1,33 @@
+import './index.css'
+import { useState } from 'react'
+
+
+export default function RowCounter() {
+    const [counter, setCounter] = useState(0)
+
+    function add() {
+        //mental note to self, ++ should not be used in react. do not write over when it comes to states just tell it what the new state is
+        setCounter(prevCounter=> prevCounter + 1)
+    }
+
+    function subtract() {
+        setCounter(prevCounter=> prevCounter - 1)
+    }
+
+    function reset() {
+        setCounter(0)
+    }
+
+    return (
+        <>
+            <h1>hey this is the counter page</h1>
+            <div className="counter">
+                <h2 className="current-count">{counter}</h2>
+                <button className="add" onClick={add}>+</button>
+                <button ClassName="subtract" onClick={subtract}>-</button>
+                <button ClassName="reset" onClick={reset}>-</button>
+            </div>
+        </>
+    )
+}
+
--- src/pages/Counter.jsx
+++ src/pages/Counter.jsx
@@ -1,13 +1,14 @@
-import './index.css'
-
 import Header from './components/Header.jsx'
 import Title from './components/Title.jsx'
 import Footer from './components/Footer.jsx'
+import RowCounter from '.components/RowCounter.jsx'
 
 export default function Counter() {
     return (
         <>
             <Header />
+            <Title />
+            <RowCounter />
             <Footer />
         </>
     )
