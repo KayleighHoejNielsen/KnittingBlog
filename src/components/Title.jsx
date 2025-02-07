eaa9c59a94c1331bfee858c7cc89cec583d9bc5f
Kayleigh Nielsen <purplekitti909@gmail.com>
2025-02-07 11:20:41 +0100

    merging to main

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,24 +2,9 @@ import '../index.css'
 import { useLocation } from 'react-router-dom'
 
 export default function Title() {
-
-    const DynamicTitle = () => {
-        const location = useLocation()
-
-        const getTitle = (pathname) => {
-            switch (pathname) {
-                case "/Counter":
-                    return "Row Counter";
-                default: "Kay's Knitting";
-            }
-        }
-    
-    return <h1>{getTitle(location.pathname)}</h1>
-    }
-
     return (
         <div className="title">
-            <DynamicTitle />
+            <h1>Kay's Knitting</h1>
         </div>
     )
 }
